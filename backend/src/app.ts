f3fe4cccbd88efe615d506427260f595a39f8f90
Towsif <ali.towsif1@gmail.com>
2024-01-29 02:44:14 +0800

    Set-up Mongo

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,10 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
 
-const port = 5000;
-
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello, World");
 });
+
+export default app;
---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+import env from "./util/validateEnv"
+import mongoose from "mongoose";
+
+const port = env.PORT;
+
+
+mongoose.connect(env.MONGO_CONNECTION_STRING!)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server started at http://localhost:${port}`);
+    });
+  }).catch(console.error);
