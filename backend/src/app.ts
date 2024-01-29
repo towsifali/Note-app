e62245b750f1e8532a6db3507dd0759aca5aabd3
Towsif <ali.towsif1@gmail.com>
2024-01-29 18:59:49 +0800

    Added Routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,25 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import router from "./routes/notes";
+import morgan from "morgan";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World");
-});
+app.use(morgan("dev"));
 
-export default app;v 
+app.use(express.json());
+
+app.use("/api/notes", router);
+
+app.use((req, res, next) => {
+  next(Error("Endpoint not found"));
+});
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction)=> {
+  console.error(error);
+  let errorMessage = "An Unknown Error Occurred";
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ message: errorMessage });
+})
+
+export default app;
--- backend/src/models/note.ts
+++ backend/src/models/note.ts
@@ -1,4 +1,4 @@
-import { InferSchemaType, Schema } from "mongoose";
+import { InferSchemaType, model, Schema } from "mongoose";
 
 const noteSchema = new Schema({
     title: { type: String, required: true },
